17b750286d4fe34e9e6f42651dbca7a9ffd6a121
Zhdanov Bogdan <z-bogdan@outlook.com>
2020-11-28 10:31:47 +0700

    Made working gui that shows the database

--- main.py
+++ main.py
@@ -1,54 +1,54 @@
-import sys
 import sqlite3
-from PyQt5 import uic  
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class MyWidget(QMainWindow):
-    def __init__(self):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(518, 521)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setObjectName("listWidget")
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.listWidget.setFont(font)
+        self.verticalLayout.addWidget(self.listWidget)
+        self.refreshbtn = QtWidgets.QPushButton(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.refreshbtn.setFont(font)
+        self.refreshbtn.setObjectName("refreshbtn")
+        self.verticalLayout.addWidget(self.refreshbtn)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.refreshbtn.clicked.connect(self.Refresh)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.refreshbtn.setText(_translate("MainWindow", "Refresh"))
+
+    def Refresh(self):
         self.db = sqlite3.connect('coffee.db')
         self.cur = self.db.cursor()
-        super().__init__()
-        uic.loadUi('main.ui', self)  
-        self.find_btn.clicked.connect(self.find)
-        self.add_btn.clicked.connect(self.add)
-        self.set_btn.clicked.connect(self.set)
+        res = self.cur.execute("""SELECT * FROM coffee""").fetchall()
+        self.listWidget.clear()
+        self.listWidget.addItems(['Name, Roast, Grounded, Taste, Price, Vollume'] + [', '.join(el) for el in res])
 
-    def add(self):
-        lst = [self.name.displayText(), self.roast.displayText(), self.beans.displayText(),
-               self.taste.displayText(), self.price.displayText(), self.volume.displayText()]
-        if all(lst):
-            zap = """INSERT INTO coffee(name,roast,ground/beans,taste,price,package_vollume) VALUES(?,?,?,?,?,?)"""
-            print(zap)
-            self.cur.execute(zap, lst)
-            self.db.commit()
-
-
-    def set(self):
-        pass
-        
-    def find(self):
-        name = self.name.displayText()
-        names = [el[0] for el in self.cur.execute('''SELECT name FROM coffee''').fetchall()]
-        if self.name.displayText() and name in names:
-            zap = """SELECT * FROM coffee WHERE name = '{}'""".format(name)
-            lst = self.cur.execute(zap).fetchall()[0]
-            self.roast.setText(lst[2])
-            self.beans.setText(lst[3])
-            self.taste.setText(lst[4])
-            self.price.setText(lst[5])
-            self.volume.setText(lst[2])
-        else:
-            self.roast.setText('None')
-            self.beans.setText('None')
-            self.taste.setText('None')
-            self.price.setText('None')
-            self.volume.setText('None')
-                
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
